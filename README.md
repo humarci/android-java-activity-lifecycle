df82825d14843f47bc19f48959cd93b1d7373c2e
Márton Huszics <MHUSZICS@hu.ibm.com>
2021-09-30 23:21:22 +0200

    ovverride methods and readme.md

--- README.md
+++ README.md
@@ -55,4 +55,54 @@ We are chechecking these functions:
 Create an empty activity app. You can run it on a device.
 
 # Step 2:
-Modify the MainActivity.java file ovveride tha activity's methods.
+Modify the `MainActivity.java` file ovveride tha activity's methods.
+Use the following package to log:
+```JAVA
+import android.util.Log;
+```
+
+Add a Log into `onCreate`:
+
+```JAVA
+Log.i("Activity1", "onCreate");
+```
+
+Ovveride the other methods with the log:
+
+```JAVA
+ @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Activity1", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Activity1", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Activity1", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Activity1", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Activity1", "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Activity1", "onRestart");
+    }
+```
--- app/src/main/java/country/mhuszics/activitylifecycledemo/MainActivity.java
+++ app/src/main/java/country/mhuszics/activitylifecycledemo/MainActivity.java
@@ -3,6 +3,7 @@ package country.mhuszics.activitylifecycledemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("Activity1", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Activity1", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Activity1", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Activity1", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Activity1", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Activity1", "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Activity1", "onRestart");
     }
 }
